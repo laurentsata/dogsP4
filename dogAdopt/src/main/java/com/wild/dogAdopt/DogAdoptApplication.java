2304899f788afe1cc1832caefb2fe7a0f5663df4
john Doe <sataward666@gmail.com>
2022-11-22 10:10:16 +0100

    pack

---
+++ dogAdopt/src/main/java/com/wild/dogAdopt/DogAdoptApplication.java
@@ -0,0 +1,13 @@
+package com.wild.dogAdopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogAdoptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogAdoptApplication.class, args);
+	}
+
+}
---
+++ dogAdopt/src/test/java/com/wild/dogAdopt/DogAdoptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wild.dogAdopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogAdoptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
